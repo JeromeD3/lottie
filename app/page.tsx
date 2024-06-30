1a9123d248d2a8d9e6b9fd402bb7614e390a1a32
邓祥柏 <1760399176@qq.com>
2024-06-30 18:04:06 +0800

    chrore: ..

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ import PoliteChicky from './PoliteChicky.json'
 import { useRef } from "react";
 export default function Home() {
   const ref = useRef<LottieRefCurrentProps>(null)
+  const data = [{ value: "hello" }, { value: "hello" }, { value: "hello" }]
   return (
     <div className="flex  min-h-screen bg-[#000] w-full max-lg:flex-col items-center justify-center p-24 ">
-      <div className="text-white flex items-center justify-center w-full   gap-2 flex-col">
-        <h1>hello word</h1>
-        <h2>hello next</h2>
+      <div className="text-white flex items-center justify-center w-full   gap-2 flex-col ">
+        {data.map((item, index) => (
+          <div className=" has-[.bg]:bg-red-200 group">
+            <h1 className="group-has-[.bg]:text-red-950" onClick={(e) => e.currentTarget.classList.add('bg')}>hello word</h1>
+          </div>
+        ))}
       </div>
 
       <Lottie onComplete={() => {
-        ref.current?.goToAndPlay(70,true)
+        ref.current?.goToAndPlay(70, true)
       }} lottieRef={ref} animationData={PoliteChicky} loop={false} />
     </div>
   );
