9562bb5e049e7b5698ea2d4104ca61927f3ba8a0
邓祥柏 <1760399176@qq.com>
2024-07-06 19:39:04 +0800

    feat: add hover 动画

---
+++ app/a/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from "react";
+
+type Coordinates = {
+  x: number;
+  y: number;
+};
+
+const GradientMaskedImage: React.FC = () => {
+  const [coords, setCoords] = useState<Coordinates>({ x: 0, y: 0 });
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    setIsVisible(true);
+
+    const rect = event.currentTarget.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left;
+    const offsetY = event.clientY - rect.top;
+
+    const percentX = ((offsetX / rect.width) * 100).toFixed(2);
+    const percentY = ((offsetY / rect.height) * 100).toFixed(2);
+
+    setCoords({ x: parseFloat(percentX), y: parseFloat(percentY) });
+  };
+
+  const handleMouseOut = () => {
+    setIsVisible(false);
+  };
+
+  const container = "relative m-auto w-[350px] h-[500px] rounded-3xl transition-all z-3"
+  const img = `absolute inset-0 rounded-3xl bg-no-repeat bg-cover bg-center bg-[url('https://oss.aiyuzhou8.com/2023/05/08-.jpg')]`
+
+  return (
+    <div className={container} onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}>
+      {isVisible && (
+        <div className={img}
+          style={{ maskImage: `radial-gradient(circle at ${coords.x}% ${coords.y}%, #000, #000, transparent, transparent, transparent)` }}
+        >
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+export default function Home() {
+  return <div className="flex justify-center items-center min-h-screen bg-white">
+    <GradientMaskedImage />
+  </div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,20 @@ export default function Home() {
   const ref = useRef<LottieRefCurrentProps>(null)
   const data = [{ value: "hello" }, { value: "hello" }, { value: "hello" }]
   return (
-    <div className="flex  min-h-screen bg-[#000] w-full max-lg:flex-col items-center justify-center p-24 ">
-      <div className="text-white flex items-center justify-center w-full   gap-2 flex-col ">
-        {data.map((item, index) => (
-          <div className=" has-[.bg]:bg-red-200 group">
-            <h1 className="group-has-[.bg]:text-red-950" onClick={(e) => e.currentTarget.classList.add('bg')}>hello word</h1>
-          </div>
-        ))}
-      </div>
+    <>
+      <div className="flex  min-h-screen bg-[#000] w-full max-lg:flex-col items-center justify-center p-24 ">
+        <div className="text-white flex items-center justify-center w-full   gap-2 flex-col ">
+          {data.map((item, index) => (
+            <div className=" has-[.bg]:bg-red-200 group" key={index}>
+              <h1 className="group-has-[.bg]:text-red-950" onClick={(e) => e.currentTarget.classList.add('bg')}>hello word</h1>
+            </div>
+          ))}
+        </div>
 
-      <Lottie onComplete={() => {
-        ref.current?.goToAndPlay(70, true)
-      }} lottieRef={ref} animationData={PoliteChicky} loop={false} />
-    </div>
+        <Lottie onComplete={() => {
+          ref.current?.goToAndPlay(70, true)
+        }} lottieRef={ref} animationData={PoliteChicky} loop={false} />
+      </div>
+    </>
   );
 }
