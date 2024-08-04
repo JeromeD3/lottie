99821dd077d0ac8fc3ef63ee97cab1f14c29f954
Jerome <1760399176@qq.com>
2024-08-04 21:15:21 +0800

    feat: 流光btn

--- app/btn/page.tsx
+++ app/btn/page.tsx
@@ -1,19 +1,25 @@
 import { Button } from '@/components/ui/button'
+import ShimmerButton from '@/components/magicui/shimmer-button'
+
 import { MoveRight } from 'lucide-react'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full h-screen flex items-center justify-center'>
-      <Button className='group  '>
-        <div className='relative  flex flex-col items-center'>
-          <p className='opacity-0'>aaaa</p>
-          <p className='absolute group-hover:-top-8 top-0 transition-all duration-300'>
-            button
-          </p>
-          <MoveRight className='absolute -bottom-8 group-hover:bottom-0 transition-all duration-300' />
+    <div className="w-full h-screen flex items-center justify-center flex-col">
+      <Button className="group  ">
+        <div className="relative  flex flex-col items-center">
+          <p className="opacity-0">aaaa</p>
+          <p className="absolute group-hover:-top-8 top-0 transition-all duration-300">button</p>
+          <MoveRight className="absolute -bottom-8 group-hover:bottom-0 transition-all duration-300" />
         </div>
       </Button>
+
+      <div className="z-10 flex min-h-[16rem] items-center justify-center">
+        <ShimmerButton className="shadow-2xl">
+          <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">Shimmer Button</span>
+        </ShimmerButton>
+      </div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,7 +33,33 @@ const config = {
         "2xl": "1400px",
       },
     },
+    keyframes: {
+      "spin-around": {
+        "0%": {
+          transform: "translateZ(0) rotate(0)",
+        },
+        "15%, 35%": {
+          transform: "translateZ(0) rotate(90deg)",
+        },
+        "65%, 85%": {
+          transform: "translateZ(0) rotate(270deg)",
+        },
+        "100%": {
+          transform: "translateZ(0) rotate(360deg)",
+        },
+      },
+      slide: {
+        to: {
+          transform: "translate(calc(100cqw - 100%), 0)",
+        },
+      },
+    },
+    animation: {
+      "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+      slide: "slide var(--speed) ease-in-out infinite alternate",
+    },
     extend: {
+
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
