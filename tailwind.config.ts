8a86384193c9c2682ec887bdeebd57e26a48bdb2
Jerome <1760399176@qq.com>
2024-08-04 21:19:44 +0800

    feat: 无限滚动格子背景

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -34,6 +34,10 @@ const config = {
       },
     },
     keyframes: {
+      grid: {
+        "0%": { transform: "translateY(-50%)" },
+        "100%": { transform: "translateY(0)" },
+      },
       "spin-around": {
         "0%": {
           transform: "translateZ(0) rotate(0)",
@@ -55,6 +59,7 @@ const config = {
       },
     },
     animation: {
+      grid: "grid 15s linear infinite",
       "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
       slide: "slide var(--speed) ease-in-out infinite alternate",
     },
