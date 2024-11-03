84bfc6f6e9599029d7fe55d5e3a5590fa8c616c3
Jerome <1760399176@qq.com>
2024-11-03 22:27:45 +0800

    bug fix

--- app/ui/SideNav.tsx
+++ app/ui/SideNav.tsx
@@ -3,7 +3,7 @@ import NavLinks from './NavLinks';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 overflow-y-auto">
       <Link
         className="mb-2 flex h-16 items-end justify-start rounded-md bg-blue-600 p-4 md:h-16"
         href="/"
